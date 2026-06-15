d428445bc8cc3c433848cba543e83fa3f591ce1e
Bruno Bohrer <bohrer.archviz@gmail.com>
2026-06-15 15:47:25 +0100

    SCROLL SEM BUG

--- app.js
+++ app.js
@@ -11,7 +11,7 @@
     lenis = new Lenis({
       duration: 1.15,
       easing: t => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      smoothWheel: true,
+      smoothWheel: false,
       smoothTouch: false
     });
     lenis.on("scroll", ScrollTrigger.update);
